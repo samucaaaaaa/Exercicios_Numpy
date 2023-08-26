4885c6dfc483017ac527a1716c859f5982e8f683
samucaaaaaa <mucaspeedpvp@gmail.com>
2023-08-26 20:59:08 -0300

    Resolução do exercicio 2

---
+++ ex_02.py
@@ -0,0 +1,6 @@
+"2- Crie uma matriz 1D com números de 0 a 9"
+
+import numpy as np
+# Podemos usar o "np.arange" para gerar uma sequência de números na array
+array_1D = np.arange(0, 10)
+print(array_1D)
